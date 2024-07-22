f6fcf3edbdf75562077330c4f6adb8682e11962b
EBC\chris.chang <chris.chang@ebc.net.tw>
2024-07-22 15:35:14 +0800

    feat: test

--- vite.config.mjs
+++ vite.config.mjs
@@ -34,7 +34,7 @@ export default defineConfig({
     }),
   ],
   define: { 'process.env': {} },
-  base:'/cueSetting/',
+  base:'/dist/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
