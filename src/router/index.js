e4ce1e2a71e0d7c5838a586bd9ff41a669643a92
EBC\chris.chang <chris.chang@ebc.net.tw>
2024-09-10 11:26:03 +0800

    feat: 20240910

--- src/router/index.js
+++ src/router/index.js
@@ -9,12 +9,53 @@
 import { createRouter, createWebHistory } from 'vue-router/auto'
 import { routes } from 'vue-router/auto-routes'
 
+import multiToRun from '@/pages/multiToRun.vue'
+import NumberImport from '@/pages/numberImport.vue'
+import AfterEvaluateSetting from '@/pages/afterEvaluateSetting.vue'
+import AfterEvaluate from '@/pages/afterEvaluate.vue'
+import Cue from '@/pages/cue.vue'
+
+const test = [
+  {
+    path: '/',
+    redirect: '/afterEvaluate',
+  },
+  {
+    path: '/afterEvaluateSetting',
+    name: '/afterEvaluateSetting',
+    component:() => AfterEvaluateSetting
+  },
+  {
+    path: '/afterEvaluate',
+    name: '/afterEvaluate',
+    component:() => AfterEvaluate
+  },
+  {
+    path: '/cue',
+    name: '/cue',
+    component: Cue
+  },
+  {
+    path: '/multiToRun',
+    name: '/multiToRun',
+    component: multiToRun
+  },
+  {
+    path: '/numberImport',
+    name: '/numberImport',
+    component: NumberImport
+  },
+  {
+    path: '/*',
+    redirect: '/afterEvaluate'
+  }
+]
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes:test
 })
-
 
 
 // Workaround for https://github.com/vitejs/vite/issues/11804
